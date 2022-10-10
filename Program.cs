773929d3207e236035063ca07790bb1224ba1455
HunterM <zakaspro@gmail.com>
2022-10-10 22:31:14 +0300

    Ищем массив с значениями в 3 символа

--- Program.cs
+++ Program.cs
@@ -3,9 +3,25 @@ Console.WriteLine("Первоначальный массив можно ввес
 Console.WriteLine("При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами");
 Console.WriteLine();
 Console.Write("Введите элементы массива через пробел и нажмите Enter: ");
-string[] arr = (Console.ReadLine().Split());
+string[] arr = (Console.ReadLine().Split()); //чтение с консоли стоки, разделителем которой является пробел
 Console.WriteLine();
-var str = string.Join(" ", arr);
+//var str = string.Join(" ", arr); //присваивание данным введеным в строке массиву str
+//Console.WriteLine(str);
+string[] FindNewArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
 
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
 
-Console.WriteLine(str);
+    return result;
+}
